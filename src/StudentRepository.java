fe843bb43c074207e66d8752ddd2f8f3543807ec
MKM-03 <mk.universityacc@gmail.com>
2026-02-13 23:59:40 +0200

    Added findByName, listAll and listByDepartment

--- src/StudentRepository.java
+++ src/StudentRepository.java
@@ -1,10 +1,19 @@
+import java.util.List;
+
 public interface StudentRepository {
 
     void saveStudent(Student student);
 
+    void deleteById(String studentId);
+
     Student findById(String studentId);
+
+    Student findByName(String name);
 
     boolean existsById(String studentId);
 
-    void deleteById(String studentId);
+    List<Student> listAll();
+
+    List<Student> listByDepartment(Department department);
+
 }
