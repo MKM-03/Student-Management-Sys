9ef2bf9a70b9304ec58c946bb1743d4f8eb28265
MKM-03 <mk.universityacc@gmail.com>
2026-02-14 22:24:17 +0200

    Created Semester record for holding info about a semester

--- src/Semester.java
+++ src/Semester.java
@@ -1,3 +1,9 @@
-public class Semester {
+import java.time.LocalDate;
 
+public record Semester(String id, String name, LocalDate startDate, LocalDate endDate) {
+    public Semester {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("Start date must before end date.");
+        }
+    }
 }
