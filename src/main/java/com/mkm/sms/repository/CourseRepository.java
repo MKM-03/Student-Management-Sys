8612ced2635377b9a1cb7751d0cfb84d588c194b
MKM-03 <mk.universityacc@gmail.com>
2026-02-24 01:45:12 +0200

    Removed unnecessary functions from CourseRepository

--- src/main/java/com/mkm/sms/repository/CourseRepository.java
+++ src/main/java/com/mkm/sms/repository/CourseRepository.java
@@ -2,24 +2,15 @@ package com.mkm.sms.repository;
 
 import com.mkm.sms.entity.Course;
 import com.mkm.sms.enums.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CourseRepository {
-
-    void saveCourse(Course course);
-
-    void deleteById(String courseId);
-
-    Course findById(String courseId);
+public interface CourseRepository extends JpaRepository<Course, String> {
 
     Course findByCode(String courseCode);
 
-    Boolean existsById(String courseId);
-
     Boolean existsByCode(String courseCode);
-
-    List<Course> listAll();
 
     List<Course> listByDepartment(Department department);
 }
